9e426688776edd619578a85ef048799edfe34d2a
escobarpsiba <escobarpsiba@gmail.com>
2026-05-01 16:01:45 -0300

    Remove build artifacts and update gitignore for Vercel

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-uMgbHFCn.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/ladingpage-signa-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-CIvF1RUy.js"] }, "/": { filePath: "C:/ladingpage-signa-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-siGpadeR.js"] } }, clientEntry: "/assets/index-CIvF1RUy.js" });
-export {
-  tsrStartManifest
-};
